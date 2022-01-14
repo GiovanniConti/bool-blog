f44b47f41350ab8675ffe8577fa0965f659158c9
20Federico <fefe.molino@gmail.com>
2022-01-14 16:30:56 +0100

    creata view e controller - show

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -50,9 +50,9 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Post $post)
     {
-        //
+      return view('posts.show', compact('post'));
     }
 
     /**
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,30 +1,25 @@
 @extends('layouts.default')
 
-@section('title', 'Homepage')
+@section('title', 'Detagli post' . $post->title)
 
 @section('page_header')
 <header>
-  <h1 class="text-center text-danger m-5">View all Post</h1>
+  <h1 class="text-center text-danger m-5">{{$post->title}}</h1>
 </header>
 @endsection
 
 @section('main_content')
     <div class="container">
-      <div class="row">
-        @foreach ($posts as $post)
-          <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="{{$post->coverImg}}" alt="Card image cap">
-            <div class="card-body">
+      <img class="card-img-top" src="{{$post->coverImg}}" alt="Card image cap">
+            <div class="">
               <h5 class="card-title">{{$post->title}}</h5>
               <p class="card-text">{{$post->subtitle}}</p>
               <p class="card-text">{{$post->content}}</p>
               <p class="card-text">{{$post->author}}</p>
               <p class="card-text">{{$post->category}}</p>
-              <a href="{{route('posts.show', $post->id)}}" class="btn btn-primary">Vedi post completo</a>
+              {{-- <a href="{{route('posts.show', $post->id)}}" class="btn btn-primary">Vedi post completo</a> --}}
             </div>
           </div>
-        @endforeach
-      </div>
     </div>
 @endsection
     
