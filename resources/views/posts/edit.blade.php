c6cc5b789e2706d2cb0b4f4e52b12c74ddfa46b9
Frameck <fra9879@gmail.com>
2022-01-14 17:32:02 +0100

    aggiunte value nel form edit

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -15,28 +15,34 @@
     
     <div class="mb-3">
       <label for="title" class="form-label">Title</label>
-      <input type="text" class="form-control" id="title" name="title">
+      <input type="text" class="form-control" id="title" name="title"
+      value="{{ $post->title }}">
     </div>
     <div class="mb-3">
       <label for="subtitle" class="form-label">Subitle</label>
-      <input type="text" class="form-control" id="subtitle" name="subtitle">
+      <input type="text" class="form-control" id="subtitle" name="subtitle"
+      value="{{ $post->subtitle }}">
     </div>
     <div class="mb-3">
       <label for="content" class="form-label">Content</label>
-      <textarea class="form-control" id="content" name="content"></textarea>
+      <textarea class="form-control" id="content" name="content"">{{ $post->content }}</textarea>
+      
     </div>
 
     <div class="mb-3">
       <label for="coverImg" class="form-label">Image</label>
-      <input type="text" class="form-control" id="coverImg" name="coverImg">
+      <input type="text" class="form-control" id="coverImg" name="coverImg"
+      value="{{ $post->coverImg }}">
     </div>
     <div class="mb-3">
       <label for="author" class="form-label">Author</label>
-      <input type="text" class="form-control" id="author" name="author">
+      <input type="text" class="form-control" id="author" name="author"
+      value="{{ $post->author }}">
     </div>
     <div class="mb-3">
       <label for="category" class="form-label">Category</label>
-      <input type="text" class="form-control" id="category" name="category">
+      <input type="text" class="form-control" id="category" name="category"
+      value="{{ $post->category }}">
     </div>
     <div class="text-center mb-3">
       <button type="submit" class="btn btn-success btn-submit">Submit</button>
