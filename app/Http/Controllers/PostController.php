584d6f5b3b689b8a7a53af182b470ab3875e1f65
20Federico <fefe.molino@gmail.com>
2022-01-14 16:44:06 +0100

    creata view e controller edit e update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -61,9 +61,9 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Post $post)
     {
-        //
+      return view('posts.edit', compact('post'));
     }
 
     /**
@@ -73,9 +73,13 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Post $post)
     {
-        //
+      $data = $request->all();
+
+      $post->update($data);
+    
+      return redirect()->route('posts.show', $post->id);
     }
 
     /**
---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.default')
+
+@section('title', 'Modifica post')
+
+@section('page_header')
+<header>
+  <h1 class="text-center text-danger m-5">Modifica Post</h1>
+</header>
+@endsection
+
+@section('main_content')
+  <form action="{{ route('posts.update', $post->id) }}" method="POST">
+    @csrf
+    @method('PUT')
+    
+    <div class="mb-3">
+      <label for="title" class="form-label">Title</label>
+      <input type="text" class="form-control" id="title" name="title">
+    </div>
+    <div class="mb-3">
+      <label for="subtitle" class="form-label">Subitle</label>
+      <input type="text" class="form-control" id="subtitle" name="subtitle">
+    </div>
+    <div class="mb-3">
+      <label for="content" class="form-label">Content</label>
+      <textarea class="form-control" id="content" name="content"></textarea>
+    </div>
+
+    <div class="mb-3">
+      <label for="coverImg" class="form-label">Image</label>
+      <input type="text" class="form-control" id="coverImg" name="coverImg">
+    </div>
+    <div class="mb-3">
+      <label for="author" class="form-label">Author</label>
+      <input type="text" class="form-control" id="author" name="author">
+    </div>
+    <div class="mb-3">
+      <label for="category" class="form-label">Category</label>
+      <input type="text" class="form-control" id="category" name="category">
+    </div>
+    <div class="text-center mb-3">
+      <button type="submit" class="btn btn-success btn-submit">Submit</button>
+    </div>
+  </form>
+
+  <script src="//js.nicedit.com/nicEdit-latest.js" type="text/javascript"></script>
+  <script type="text/javascript">bkLib.onDomLoaded(nicEditors.allTextAreas);</script>
+@endsection
