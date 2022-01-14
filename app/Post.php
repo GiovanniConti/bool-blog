0257c169aab56b710d5920822e8c5ef3236fdf31
GiovanniConti <giovanni.conti@live.it>
2022-01-14 13:16:20 +0100

    added create view

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [ 
+    'title',
+    'subtitle',
+    'content',
+    'author',
+    'coverImg',
+    'category',
+    ];
 }
